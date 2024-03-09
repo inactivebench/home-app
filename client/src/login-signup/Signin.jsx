0bcaaf1ad57c90f33def47c320d5c348b6fb5c56
inactivebench <emmanuelmwangi86@gmail.com>
2024-03-09 03:22:06 +0300

    added password auth on signin and JWT auth

--- client/src/login-signup/Signin.jsx
+++ client/src/login-signup/Signin.jsx
@@ -1,13 +1,12 @@
 import { useRef, useState, useEffect, useContext } from 'react';
-import AuthContext from "../context/AuthProvider";
 import {Link} from 'react-router-dom'
 
 
 import axios from 'axios';
-// const SIGNIN_URL = '/auth';
+const SIGNIN_URL = 'http://localhost:5000/signin';
+axios.defaults.withCredentials = true;
 
 const Signin = () => {
-    const { setAuth } = useContext(AuthContext);
     const emailRef = useRef();
     const errRef = useRef();
 
@@ -15,6 +14,7 @@ const Signin = () => {
     const [pwd, setPwd] = useState('');
     const [errMsg, setErrMsg] = useState('');
     const [success, setSuccess] = useState(false);
+    const [loginStatus, setLoginStatus] = useState(false);
 
     useEffect(() => {
         emailRef.current.focus();
@@ -24,18 +24,37 @@ const Signin = () => {
         setErrMsg('');
     }, [email, pwd])
 
+    useEffect(() => {
+        axios.get("http://localhost:5000/signin").then((response) => {
+            console.log(response);
+          if (response.data.loggedIn == true) {
+            setRole(response.data.user[0].role);
+          }
+        });
+      }, []);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         try {
-            // const response = await axios.post(SIGNIN_URL,
-            //     JSON.stringify({ email, pwd }),
-            //     {
-            //         headers: { 'Content-Type': 'application/json' },
-            //         withCredentials: true
-            //     }
-            // );
-            // console.log(JSON.stringify(response?.data))
+            const response = await axios.post(SIGNIN_URL,
+                JSON.stringify({ customer_email: email, password: pwd }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            ) .then((response) => {
+                if (!response.data.auth) {
+                   setLoginStatus(false);
+                } else {
+                   console.log(response.data);
+                   localStorage.setItem("token", response.data.token)
+                   setLoginStatus (true);
+                }
+              });
+           
+            
+            console.log(JSON.stringify(response?.data))
             // const accessToken = response?.data?.accessToken;
             // const roles = response?.data?.roles;
             // setAuth({ email, pwd, roles, accessToken });
--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
 const express = require("express");
 const mysql = require('mysql2')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const jwt = require('jsonwebtoken');
+
 const cors = require('cors')
 const app = express();
+
 // Middleware to parse JSON bodies
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin : "http://localhost:5173",
-//     methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
-//     credentials: true,
-//     allowedHeaders: ['Content-Type', 'Authorization']
-//   })
-// )
-// app.options('*', cors()) 
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
@@ -23,7 +20,20 @@ app.use((req, res, next) => {
   next();
 });
 
-
+// cookieParser middleware
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use (
+    session ({
+        key: "userId",
+        secret: "subscribe",
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            expires: 60 * 60 * 24,
+        },
+    })
+);
 
 
 // create db connection
@@ -45,7 +55,7 @@ console.log("mysql connected");
 
 
  // api route
-
+// signup
   app.post('/signup', (req, res)=>{
     
     let user = {
@@ -65,7 +75,74 @@ console.log("mysql connected");
     })  
   })
 
+ //JWT verification
+ const verifyJWT = (req, res, next) => {
+  const token = req.headers["x-access-token"];    if (!token) {
+      res.send("We need a token, please give it to us next time");
+  } else {
+      jwt.verify(token, "jwtSecret", (err, decoded) => {
+          if (err) {
+              console.log(err);
+              res.json({ auth: false, message: "you are failed to authenticate"});
+          } else {
+              req.userId = decoded.id;
+              next();
+          }
+      });
+  }
+};
+
+  // sign in authorization
+  app.post('/signin', (req, res)=>{
+
+    const customer_email = req.body.customer_email
+    const password = req.body.password
+    let sql = ' SELECT * FROM customers WHERE customer_email = ? AND password = ?  ';
+    let query = db.query(sql, [customer_email, password], (err, result)=>{
+      if(err){
+        throw err
+      }
+      
+      if(result.length > 0){
+        const id = result[0].id
+        const token = jwt.sign({id}, "jwtSecret", {expiresIn: 300,})
+        req.session.user = result;
+        console.log(req.session.user);
+        res.json({auth: true, token: token, result: result});
+        // res.send(result)
+        console.log('SUCCESS !! ')
+      }else{
+        console.log('wrong email/password ')
+        res.json({auth: false, message: "no user exists"})
+      }
+
+})  
+  })
+
  
+
+  // sign in session
+  app.get("/signin", (req, res) => {
+    if (req.session.user) {
+      res.send({ loggedIn: true, user: req.session.user });
+    } else {
+      res.send({ loggedIn: false });
+    }
+  });
+
+  // check user authentication
+  app.get('/isUserAuth', verifyJWT , (req, res) => {
+    res.send("You are authenticated Congrats:")
+})
+
+
+
+
+
+
+
+
+
 
   // select users
   app.get('/getusers', (req, res)=>{
