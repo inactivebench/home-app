db93244b0ce3ab11242500bf9439cfc9d7c11fb9
inactivebench <emmanuelmwangi86@gmail.com>
2024-03-25 01:10:19 +0300

    search and pagination added

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, useMemo } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { LoginContext } from "../context/LoginContext";
 import {
@@ -7,20 +7,24 @@ import {
   faBed,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Pagination from "../components/Pagination";
 import axios from "axios";
 // import Card from "../components/Card";
+
+let PageSize = 10;
 
 const Search = () => {
   const [sidebardata, setSidebardata] = useState({
     searchTerm: "",
     type: "all",
+    category: "all",
     minPrice: "",
     maxPrice: "",
   });
   const [loading, setLoading] = useState(false);
   const [listings, setListings] = useState([]);
-  const [showMore, setShowMore] = useState(false);
   const { isLoggedIn } = useContext(LoginContext);
+  const [currentPage, setCurrentPage] = useState(1);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -35,28 +39,28 @@ const Search = () => {
     const urlParams = new URLSearchParams(location.search);
     const searchTermFromUrl = urlParams.get("searchTerm");
     const typeFromUrl = urlParams.get("type");
+    const categoryFromUrl = urlParams.get("category");
     const minPriceFromUrl = urlParams.get("minPrice");
     const maxPriceFromUrl = urlParams.get("maxPrice");
 
     if (
       searchTermFromUrl ||
       typeFromUrl ||
+      categoryFromUrl ||
       minPriceFromUrl ||
       maxPriceFromUrl
     ) {
       setSidebardata({
         searchTerm: searchTermFromUrl || "",
         type: typeFromUrl || "all",
+        category: categoryFromUrl || "all",
         minPrice: minPriceFromUrl || "",
         maxPrice: maxPriceFromUrl || "",
-
-        //  parking: parkingFromUrl === "true" ? true : false,
       });
     }
 
     const fetchListings = async () => {
       setLoading(true);
-      setShowMore(false);
       const searchQuery = urlParams.toString();
       try {
         const response = await axios(
@@ -64,13 +68,6 @@ const Search = () => {
         );
         const data = response.data;
 
-        // const res = await fetch(`/api/listing/get?${searchQuery}`);
-        // const data = await res.json();
-        if (data.length > 10) {
-          setShowMore(true);
-        } else {
-          setShowMore(false);
-        }
         setListings(data);
         setLoading(false);
       } catch (error) {
@@ -82,12 +79,18 @@ const Search = () => {
   }, [location.search]);
 
   const handleChange = (e) => {
-    if (
-      e.target.id === "all" ||
-      e.target.id === "rent" ||
-      e.target.id === "sale"
-    ) {
-      setSidebardata({ ...sidebardata, type: e.target.id });
+    // if (
+    //   e.target.id === "all" ||
+    //   e.target.id === "rent" ||
+    //   e.target.id === "sale"
+    // ) {
+    //   setSidebardata({ ...sidebardata, type: e.target.id });
+    // }
+    if (e.target.id === "type") {
+      setSidebardata({ ...sidebardata, type: e.target.value });
+    }
+    if (e.target.id === "category") {
+      setSidebardata({ ...sidebardata, category: e.target.value });
     }
 
     if (e.target.id === "searchTerm") {
@@ -106,6 +109,7 @@ const Search = () => {
     const urlParams = new URLSearchParams();
     urlParams.set("searchTerm", sidebardata.searchTerm);
     urlParams.set("type", sidebardata.type);
+    urlParams.set("category", sidebardata.category);
     urlParams.set("minPrice", sidebardata.minPrice);
     urlParams.set("maxPrice", sidebardata.maxPrice);
     const searchQuery = urlParams.toString();
@@ -121,14 +125,18 @@ const Search = () => {
     const response = await axios(
       `http://localhost:5000/api/property/properties?${searchQuery}`
     );
+
     const data = response.data;
-    // const res = await fetch(`/api/listing/get?${searchQuery}`);
-    // const data = await res.json();
     if (data.length < 10) {
       setShowMore(false);
     }
     setListings([...listings, ...data]);
   };
+  const propertyData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return listings.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage]);
   return (
     <div className='flex flex-col md:flex-row'>
       <div className='p-7  border-b-2 md:border-r-2 md:min-h-screen'>
@@ -151,7 +159,27 @@ const Search = () => {
           </div>
           <div className='flex gap-2 flex-wrap items-center'>
             <label className='font-semibold'>Type:</label>
-            <div className='flex gap-2'>
+            <select
+              id='type'
+              className='px-2 py-1 border rounded'
+              onChange={handleChange}
+            >
+              <option value='all'>Rent & Sale</option>
+              <option value='rent'>Rent</option>
+              <option value='sale'>Sale</option>
+            </select>
+
+            <select
+              id='category'
+              className='px-2 py-1 border rounded'
+              onChange={handleChange}
+            >
+              <option value='all'>All</option>
+              <option value='apartment'>Apartment</option>
+              <option value='house'>House</option>
+            </select>
+
+            {/* <div className='flex gap-2'>
               <input
                 type='checkbox'
                 id='all'
@@ -180,7 +208,7 @@ const Search = () => {
                 checked={sidebardata.type === "sale"}
               />
               <span>Sale</span>
-            </div>
+            </div> */}
           </div>
           <div className='flex gap-2 flex-wrap items-center'>
             <label className='font-semibold'>Price:</label>
@@ -203,6 +231,17 @@ const Search = () => {
               />
             </div>
           </div>
+          <select
+            onChange={handleChange}
+            defaultValue={"created_at_desc"}
+            id='sort_order'
+            className='border rounded-lg p-3'
+          >
+            <option value='regularPrice_desc'>Price high to low</option>
+            <option value='regularPrice_asc'>Price low to hight</option>
+            <option value='createdAt_desc'>Latest</option>
+            <option value='createdAt_asc'>Oldest</option>
+          </select>
           <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>
             Search
           </button>
@@ -229,16 +268,16 @@ const Search = () => {
             ))} */}
           {!loading &&
             listings &&
-            listings.map((listing) => (
+            propertyData.map((listing) => (
               <Link to={`/listing/${listing.property_id}`}>
                 <div className='mt-8 text-black w-full max-w-[28rem] mx-auto min-h-full  p-2 border rounded-lg border-black'>
                   <div
                     className='w-full max-w-[28rem] mx-auto min-h-full '
                     key={listing.property_id}
                   >
-                    <div className='listing-header  mb-3 '>
+                    <div className='w-full   mb-3 '>
                       <img
-                        className='rounded-md'
+                        className='h-[400px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300 rounded-md'
                         src={listing.property_image_url}
                         alt='apartment image'
                       />
@@ -271,16 +310,32 @@ const Search = () => {
                         <p className='text-sm'>
                           <FontAwesomeIcon
                             icon={faLocationDot}
-                            className='pr-2'
+                            className='text-green-600 pr-2 '
                           />
                           {listing.property_location}
                         </p>
-                        <h2 className='capitalize text-slate-500'>
-                          <span className='text-blue-700 text-2xl font-bold '>
-                            Kshs {listing.property_price}
-                          </span>
-                          per month
-                        </h2>
+                        {listing.property_type === "Rent" ? (
+                          <h2 className='capitalize text-slate-500'>
+                            <span className='text-blue-500 text-2xl font-bold mr-2 '>
+                              Kshs{" "}
+                              {listing.property_price.replace(
+                                /\B(?=(\d{3})+(?!\d))/g,
+                                ","
+                              )}
+                            </span>
+                            / month
+                          </h2>
+                        ) : (
+                          <h2 className='capitalize text-slate-500'>
+                            <span className='text-blue-500 text-2xl font-bold mr-2 '>
+                              Kshs{" "}
+                              {listing.property_price.replace(
+                                /\B(?=(\d{3})+(?!\d))/g,
+                                ","
+                              )}
+                            </span>
+                          </h2>
+                        )}
                       </article>
                       <hr className='h-px my-3 bg-gray-200 border dark:bg-gray-700'></hr>
                       <article className='more-listing-info'>
@@ -305,14 +360,13 @@ const Search = () => {
               </Link>
             ))}
 
-          {showMore && (
-            <button
-              onClick={onShowMoreClick}
-              className='text-green-700 hover:underline p-7 text-center w-full'
-            >
-              Show more
-            </button>
-          )}
+          <Pagination
+            className='w-full flex justify-center mt-8'
+            currentPage={currentPage}
+            totalCount={listings.length}
+            pageSize={PageSize}
+            onPageChange={(page) => setCurrentPage(page)}
+          />
         </div>
       </div>
     </div>
--- server/routes/propertyRoutes.js
+++ server/routes/propertyRoutes.js
@@ -4,19 +4,16 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
-router.get("/properties", (req, res) => {
-  const limit = parseInt(req.query.limit) || 9;
-  const startIndex = parseInt(req.query.startIndex) || 0;
-
-  let sql = " SELECT * FROM property ";
-  let query = db.query(sql, (err, result) => {
-    if (err) {
-      throw err;
-    }
-    console.log("property data retrieved ");
-    res.send(result);
-  });
-});
+// router.get("/properties", (req, res) => {
+//   let sql = " SELECT * FROM property ";
+//   let query = db.query(sql, (err, result) => {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log("property data retrieved ");
+//     res.send(result);
+//   });
+// });
 
 router.get("/listing/:id", (req, res) => {
   let sql = `SELECT * FROM property WHERE property_id = ${req.params.id} `;
@@ -28,5 +25,42 @@ router.get("/listing/:id", (req, res) => {
     res.send(result);
   });
 });
+
+const getListings = async (req, res, next) => {
+  try {
+    const type = req.query.type || "all";
+    const category = req.query.category || "all";
+    const minPrice = parseInt(req.query.minPrice) || 0;
+    const maxPrice = parseInt(req.query.maxPrice) || Infinity;
+    const searchTerm = req.query.searchTerm || "";
+
+    let sql = `SELECT * FROM property WHERE property_location LIKE '%${searchTerm}%'`;
+
+    if (type !== "all") {
+      sql += ` AND property_type = '${type}'`;
+    }
+
+    if (category !== "all") {
+      sql += ` AND property_category = '${category}'`;
+    }
+
+    if (maxPrice !== Infinity) {
+      sql += ` AND property_price BETWEEN ${minPrice} AND ${maxPrice}`;
+    } else {
+      sql += ` AND property_price >= ${minPrice}`;
+    }
+
+    db.query(sql, (err, result) => {
+      if (err) {
+        throw err;
+      }
+      res.status(200).json(result);
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+router.get("/properties", getListings);
 
 module.exports = router;
